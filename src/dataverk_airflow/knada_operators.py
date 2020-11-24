359b8d7da0dabeadd206fd4fcd95b142c54e8aa7
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-11-24 09:46:15 +0100

    add dbt seed operator

--- src/dataverk_airflow/knada_operators.py
+++ src/dataverk_airflow/knada_operators.py
@@ -7,7 +7,8 @@ from airflow.kubernetes.volume import Volume
 from airflow.kubernetes.volume_mount import VolumeMount
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
-from dataverk_airflow.init_containers import create_git_clone_init_container, change_permissions_init_container
+from dataverk_airflow.init_containers import create_git_clone_init_container, change_permissions_init_container, \
+    dbt_read_gcs_bucket
 from dataverk_airflow.notifications import create_email_notification, create_slack_notification
 
 
@@ -182,3 +183,106 @@ def create_knada_python_pod_operator(
         retries=retries,
         retry_delay=retry_delay,
     )
+
+
+def create_knada_dbt_seed_operator(
+    dag: DAG,
+    name: str,
+    repo: str,
+    namespace: str,
+    dbt_dir: str,
+    seed_source: dict = None,
+    email: str = None,
+    slack_channel: str = None,
+    branch: str = "master",
+    resources: dict = None,
+    retries: int = 3,
+    delete_on_finish: bool = True,
+    retry_delay: timedelta = timedelta(seconds=5),
+):
+    """ Factory function for creating KubernetesPodOperator for executing knada python scripts
+
+    :param dag: DAG: owner DAG
+    :param name: str: Name of task
+    :param repo: str: Github repo
+    :param dbt_dir: str: Path to dbt directory
+    :param seed_source: dict: Dictionary with keys for bucket_name (name of gcs bucket name) and blob_name (name of blob in bucket)
+    :param namespace: str: K8S namespace for pod
+    :param email: str: Email of owner
+    :param slack_channel: Name of slack channel, default None (no slack notification)
+    :param branch: str: Branch in repo, default "master"
+    :param resources: dict: Specify required cpu and memory requirements (keys in dict: request_memory, request_cpu, limit_memory, limit_cpu), default None
+    :param retries: int: Number of retries for task before DAG fails, default 3
+    :param delete_on_finish: bool: Whether to delete pod on completion
+    :param retry_delay: timedelta: Time inbetween retries, default 5 seconds
+    :return: KubernetesPodOperator
+    """
+
+    def on_failure(context):
+        if email:
+            send_email = create_email_notification(email, name, namespace, dag)
+            send_email.execute(context)
+
+        if slack_channel:
+            slack_notification = create_slack_notification(slack_channel, name, namespace)
+            slack_notification.execute(context)
+
+    return KubernetesPodOperator(
+        init_containers=[create_git_clone_init_container(repo, branch, "/repo"),
+                         dbt_read_gcs_bucket("repo", seed_source, dbt_dir),
+                         change_permissions_init_container("/repo")],
+        dag=dag,
+        on_failure_callback=on_failure,
+        name=name,
+        namespace=namespace,
+        task_id=name,
+        is_delete_operator_pod=delete_on_finish,
+        image=os.getenv("KNADA_PYTHON_POD_OP_IMAGE", "navikt/knada-dbt:1"),
+        env_vars={
+            "GOOGLE_APPLICATION_CREDENTIALS": "/home/jovyan/creds.json",
+            "HTTPS_PROXY": os.environ["HTTPS_PROXY"],
+            "https_proxy": os.environ["HTTPS_PROXY"],
+            "NO_PROXY": os.environ["NO_PROXY"],
+            "no_proxy": os.environ["NO_PROXY"],
+        },
+        cmds=["dbt", "seed"],
+        arguments=["--profiles-dir", dbt_dir, "--project-dir", dbt_dir],
+        volume_mounts=[
+            VolumeMount(
+                name="dags-data", mount_path="/repo", sub_path=None, read_only=False
+            ),
+            VolumeMount(
+                name="google-creds",
+                mount_path="/home/jovyan",
+                sub_path=None,
+                read_only=False,
+            ),
+        ],
+        service_account_name="airflow",
+        volumes=[
+            Volume(name="dags-data", configs={}),
+            Volume(
+                name="git-clone-secret",
+                configs={
+                    "secret": {
+                        "defaultMode": 448,
+                        "secretName": os.environ["K8S_GIT_CLONE_SECRET"],
+                    }
+                },
+            ),
+            Volume(
+                name="google-creds",
+                configs={
+                    "secret": {
+                        "defaultMode": 448,
+                        "secretName": os.getenv("K8S_GOOGLE_CREDS", "google-creds"),
+                    }
+                },
+            ),
+        ],
+        annotations={"sidecar.istio.io/inject": "false"},
+        resources=resources,
+        retries=retries,
+        retry_delay=retry_delay,
+    )
+
