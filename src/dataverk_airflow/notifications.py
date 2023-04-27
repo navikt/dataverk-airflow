877a014b95e39febb28f621c16eacf95283879d0
Erik Vattekar <erik.vattekar@nav.no>
2023-04-27 10:37:05 +0200

    switch to SlackAPIPostOperator

--- src/dataverk_airflow/notifications.py
+++ src/dataverk_airflow/notifications.py
@@ -2,7 +2,7 @@ import os
 
 from typing import Sequence, Union, List
 
-from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
 from airflow.operators.email import EmailOperator
 from datetime import datetime
 
@@ -18,13 +18,13 @@ def create_email_notification(dag_id: str, email: Union[List[str], str], name: s
             )
 
 
-def create_slack_notification(dag_id: str, slack_channel: str, name: str, namespace: str):
-    return SlackWebhookOperator(
-        task_id="airflow_task_failed",
-        http_conn_id=None,
-        webhook_token=os.environ["SLACK_TOKEN"],
-        message=f"@here Airflow task {name} i DAG {dag_id} feilet i namespace {namespace} kl. {datetime.now().isoformat()}. ",
-        channel=slack_channel,
-        link_names=True,
-        icon_emoji=":sadpanda:",
+def create_slack_notification(dag_id: str, slack_channel: str, name: str, namespace: str, dag):
+    return SlackAPIPostOperator(
+            task_id="airflow_task_failed",
+            dag=dag,
+            token=os.environ["SLACK_TOKEN"],
+            text=f"@here Airflow task {name} i DAG {dag_id} feilet i namespace {namespace} kl. {datetime.now().isoformat()}.",
+            channel=slack_channel,
+            link_names=True,
+            icon_emoji=":sadpanda:",
     )
