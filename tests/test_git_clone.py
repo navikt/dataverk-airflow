8365826780f7bc6640a155f56d9dd280f19b03a9
Kyrre Havik <kyrremann@gmail.com>
2023-10-10 16:22:51 +0200

    feat: enkel test for git clone

---
+++ tests/test_git_clone.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+import yaml
+from dataverk_airflow import git_clone
+
+FIXTURE_DIR = Path(__file__).parent.resolve() / 'test_files'
+
+
+@pytest.mark.datafiles(FIXTURE_DIR / "git_clone.yaml")
+def test_git_clone(datafiles):
+    container = git_clone("repo", "branch", "mount")
+    for yml in datafiles.iterdir():
+        with open(yml) as f:
+            assert container.to_dict() == yaml.safe_load(f.read())
