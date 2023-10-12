b4c355c643382f86d46082037c6fb4bd0da6ff78
Kyrre Havik <kyrremann@gmail.com>
2023-10-12 16:00:34 +0200

    feat: bruke fixture for dummy data

--- tests/test_kubernetes_operator.py
+++ tests/test_kubernetes_operator.py
@@ -1,10 +1,10 @@
 import datetime
 import os
-import pytest
-
-from airflow import DAG
-from dataverk_airflow import kubernetes_operator, MissingValueException
 from unittest import mock
+
+import pytest
+from airflow import DAG
+from dataverk_airflow import MissingValueException, kubernetes_operator
 
 
 @mock.patch.dict(os.environ, {"KNADA_TEAM_SECRET": "team-secret",
@@ -13,20 +13,21 @@ from unittest import mock
 class TestKubernetesOperator:
     """Test kubernetes_operator.py"""
 
-    def test_that_exception_is_raised_for_empty_image(self):
+    @pytest.fixture
+    def dag(self):
+        return DAG("dag_id", start_date=datetime.datetime(2023, 10, 10))
+
+    def test_that_exception_is_raised_for_empty_image(self, dag):
         with pytest.raises(MissingValueException) as info:
-            dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 10))
             kubernetes_operator(dag, "name", "repo", "")
         assert "image cannot be empty" in str(info.value)
 
-    def test_that_exception_is_raised_for_empty_repo(self):
+    def test_that_exception_is_raised_for_empty_repo(self, dag):
         with pytest.raises(MissingValueException) as info:
-            dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 10))
             kubernetes_operator(dag, "name", "", "image")
         assert "repo cannot be empty" in str(info.value)
 
-    def test_that_extra_envs_are_merged_in(self):
-        dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 10))
+    def test_that_extra_envs_are_merged_in(self, dag):
         container = kubernetes_operator(dag, "name", "repo", "image",
                                         extra_envs={"key": "value"})
         test_value = None
@@ -35,8 +36,7 @@ class TestKubernetesOperator:
                 test_value = env.value
         assert test_value == "value"
 
-    def test_that_slack_is_added_to_allowlist(self):
-        dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 10))
+    def test_that_slack_is_added_to_allowlist(self, dag):
         container = kubernetes_operator(dag, "name", "repo", "image",
                                         slack_channel="slack")
 
--- tests/test_python_operator.py
+++ tests/test_python_operator.py
@@ -2,6 +2,7 @@ import datetime
 import os
 from unittest import mock
 
+import pytest
 from airflow import DAG
 from dataverk_airflow.python_operator import python_operator
 
@@ -13,17 +14,18 @@ from dataverk_airflow.python_operator import python_operator
 class TestPythonOperator:
     """Test python_operator.py"""
 
-    def test_that_knakda_operator_image_is_used(self):
-        dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 11))
+    @pytest.fixture
+    def dag(self):
+        return DAG("dag_id", start_date=datetime.datetime(2023, 10, 10))
+
+    def test_that_knada_operator_image_is_used(self, dag):
         container = python_operator(dag, "name", "repo", "script_path")
         assert container.image == "operator-image"
 
-    def test_that_personal_operator_image_is_used(self):
-        dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 11))
+    def test_that_personal_operator_image_is_used(self, dag):
         container = python_operator(dag, "name", "repo", "script_path", image="personal-image")
         assert container.image == "personal-image"
 
-    def test_that_cmds_are_correct(self):
-        dag = DAG("dag_id", start_date=datetime.datetime(2023, 10, 11))
+    def test_that_cmds_are_correct(self, dag):
         container = python_operator(dag, "name", "repo", "script_path")
-        assert container.cmds[-1] == "script_path"
+        assert container.cmds == ["python", "script_path"]
