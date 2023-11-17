6fe58bdd2c7a70b9461383f0ac84beb8cac538a6
Erik Vattekar <erik.vattekar@nav.no>
2023-11-17 10:50:02 +0100

    fix: regex

--- dataverk_airflow/bucket_read.py
+++ dataverk_airflow/bucket_read.py
@@ -18,7 +18,7 @@ def bucket_read(
         ],
         command=["/bin/sh", "-c"],
         args=[
-            f"gsutil rsync -r -x '^logs/*|.*-build/*' gs://{os.environ['GCS_BUCKET']} {mount_path} && chmod -R 777 {mount_path}"
+            f"gsutil rsync -r -x '^logs/*|^.*-build/*' gs://{os.environ['GCS_BUCKET']} {mount_path} && chmod -R 777 {mount_path}"
         ],
         resources=k8s.V1ResourceRequirements(
             requests={"memory": "128Mi"},
