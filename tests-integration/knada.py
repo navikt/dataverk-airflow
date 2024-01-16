cc27fa935b5351a5c3e827ab63098e2e498b0591
Erik Vattekar <erik.vattekar@nav.no>
2024-01-16 10:35:42 +0100

    add quarto book test

--- tests-integration/knada.py
+++ tests-integration/knada.py
@@ -5,34 +5,50 @@ from dataverk_airflow import python_operator, notebook_operator, quarto_operator
 
 
 with DAG('KnadaOperators', start_date=datetime(2023, 2, 15), schedule=None) as dag:
-    py_op = python_operator(
-        dag=dag,
-        name="python-op",
-        repo="navikt/dataverk-airflow",
-        script_path="tests-integration/notebooks/script.py",
-        requirements_path="tests-integration/notebooks/requirements.txt",
-        retries=0,
-        startup_timeout_seconds=60,
-    )
+    # py_op = python_operator(
+    #     dag=dag,
+    #     name="python-op",
+    #     repo="navikt/dataverk-airflow",
+    #     script_path="tests-integration/notebooks/script.py",
+    #     requirements_path="tests-integration/notebooks/requirements.txt",
+    #     retries=0,
+    #     startup_timeout_seconds=60,
+    # )
 
-    nb_op = notebook_operator(
-        dag=dag,
-        name="nb-op",
-        repo="navikt/dataverk-airflow",
-        nb_path="tests-integration/notebooks/mynb.ipynb",
-        requirements_path="tests-integration/notebooks/requirements.txt",
-        retries=0,
-        startup_timeout_seconds=60,
-    )
+    # nb_op = notebook_operator(
+    #     dag=dag,
+    #     name="nb-op",
+    #     repo="navikt/dataverk-airflow",
+    #     nb_path="tests-integration/notebooks/mynb.ipynb",
+    #     requirements_path="tests-integration/notebooks/requirements.txt",
+    #     retries=0,
+    #     startup_timeout_seconds=60,
+    # )
 
-    quarto_op = quarto_operator(
+    # quarto_op = quarto_operator(
+    #     dag=dag,
+    #     name="quarto-op",
+    #     repo="navikt/dataverk-airflow",
+    #     quarto={
+    #         "path": "tests-integration/notebooks/quarto.ipynb",
+    #         "env": "dev",
+    #         "id": "bf48d8a4-05ca-47a5-a360-bc24171baf62",
+    #         "token": Variable.get("quarto_token"),
+    #     },
+    #     requirements_path="tests-integration/notebooks/requirements.txt",
+    #     retries=0,
+    #     startup_timeout_seconds=60,
+    # )
+
+    quarto_book_op = quarto_operator(
         dag=dag,
-        name="quarto-op",
+        name="quarto-book-op",
         repo="navikt/dataverk-airflow",
+        branch="quarto-books-test",
         quarto={
-            "path": "tests-integration/notebooks/quarto.ipynb",
+            "folder": "tests-integration/notebooks/quartobook",
             "env": "dev",
-            "id": "bf48d8a4-05ca-47a5-a360-bc24171baf62",
+            "id": "757da08e-031e-4fac-a5f0-fffe6d2d96b6",
             "token": Variable.get("quarto_token"),
         },
         requirements_path="tests-integration/notebooks/requirements.txt",
@@ -40,6 +56,7 @@ with DAG('KnadaOperators', start_date=datetime(2023, 2, 15), schedule=None) as d
         startup_timeout_seconds=60,
     )
 
-    py_op
-    nb_op
-    quarto_op
+    #py_op
+    #nb_op
+    #quarto_op
+    quarto_book_op
